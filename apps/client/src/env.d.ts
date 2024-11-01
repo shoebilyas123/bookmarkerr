c14825041a5da251a9cc9009e7313e11bf53a0a8
shoebilyas123 <shoebilyas123@gmail.com>
2024-11-01 21:13:30 +0530

    fix vite env names

--- apps/client/src/env.d.ts
+++ apps/client/src/env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_API_KEY: string;
-  readonly SERVER_URL: string;
+  readonly VITE_SERVER_URL: string;
 }
 
 interface ImportMeta {
