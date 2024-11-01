31b339894b344a8f1275e0214a0b523b44ebd3d3
shoebilyas123 <shoebilyas123@gmail.com>
2024-11-01 21:13:04 +0530

    fix vite env names

--- apps/client/src/lib/api.ts
+++ apps/client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const BASE_API =
-  import.meta.env.SERVER_URL || 'http://localhost:8000/api/v1';
+  import.meta.env.VITE_SERVER_URL || 'http://localhost:8000/api/v1';
 
 const instance = axios.create({ baseURL: BASE_API });
 
