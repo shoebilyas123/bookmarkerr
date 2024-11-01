77fcd7542ad10de42ae2f6061ebb0438ef7703c5
shoebilyas123 <shoebilyas123@gmail.com>
2024-11-01 20:21:06 +0530

    fix: client redirect to portal from home

--- apps/client/src/routes.tsx
+++ apps/client/src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import InDev from './pages/errors/InDev';
 import ProtectedLayout from './components/custom/layout';
 import PortalHome from './pages/portal';
@@ -9,6 +9,12 @@ import RegisterPage from './pages/auth/register';
 import MyProfile from './pages/portal/my-account/profile';
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: () => {
+      return <Navigate to="/portal" />;
+    },
+  },
   {
     path: '/portal/my-account/*',
     element: (
